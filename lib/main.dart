9f5c6935665cdb9a2e9517b38148f034490a02e7
yasin <yasinmiapolash@gmail.com>
2023-10-13 23:08:33 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,94 @@
 import 'package:flutter/material.dart';
-import 'package:moudel10/home_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomeScreen(),
+      home: ItemSelectionScreen(),
     );
   }
+}
+
+class ItemSelectionScreen extends StatefulWidget {
+  @override
+  _ItemSelectionScreenState createState() => _ItemSelectionScreenState();
+}
+
+class _ItemSelectionScreenState extends State<ItemSelectionScreen> {
+  List<ItemModel> items = [
+    ItemModel("Item 1", false),
+    ItemModel("Item 2", false),
+    ItemModel("Item 3", false),
+    ItemModel("Item 4", false),
+    ItemModel("Item 5", false),
+  ];
+
+  int selectedCount = 0;
+
+  void toggleItemSelection(int index) {
+    setState(() {
+      items[index].isSelected = !items[index].isSelected;
+      if (items[index].isSelected) {
+        selectedCount++;
+      } else {
+        selectedCount--;
+      }
+    });
+  }
+
+  void showSelectionDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Selected Items"),
+          content: Text("Number of  selected item: $selectedCount "),
+          actions: <Widget>[
+            TextButton(
+              child: Text("Close"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(" Selection Screen"),
+      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(items[index].name),
+            onTap: () {
+              toggleItemSelection(index);
+            },
+            tileColor: items[index].isSelected ? Colors.blue : null,
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showSelectionDialog();
+        },
+        child: Icon(Icons.check),
+      ),
+    );
+  }
+}
+
+class ItemModel {
+  final String name;
+  bool isSelected;
+
+  ItemModel(this.name, this.isSelected);
 }
